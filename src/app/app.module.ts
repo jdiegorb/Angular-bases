780de01ef110a75bd57eb86452c661a6d91343c3
jdrodriguezb <juan.rodriguezb.contractor@forum.cl>
2024-04-20 20:53:12 -0400

    avance module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     HeroComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
     AppRoutingModule
   ],
   providers: [],
